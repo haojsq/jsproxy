ba0f54864f651039ac7c728cb3f98c0c891ece4d
haojsq <51290666+haojsq@users.noreply.github.com>
2019-12-06 14:12:07 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -29,7 +29,10 @@ jsproxy_config({
     'mysite': {
       label: '当前站点',
       lines: {
-        [location.host]: 1,
+        'b.007.workers.dev': 1,
+        'b.hehe.workers.dev': 1,
+        'b.lulu.workers.dev': 1,
+        'b.jsproxy.workers.dev': 1,
       }
     },
     // 该节点用于加载大体积的静态资源
